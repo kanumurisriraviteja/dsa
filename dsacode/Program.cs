95d94b63603922371b1ed39abcf49c3a6e8f0446
Kanumuri Sri Ravi Teja <ksriraviteja20@outlook.com>
2024-01-31 16:50:35 +0530

    Base solution

---
+++ dsacode/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace dsacode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start....................");
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
